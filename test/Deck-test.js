ea6f37dfdd18a175124bb47b800d3a81cf724f9c
Maya Kappen <mayakappen@gmail.com>
2022-07-06 17:06:15 -0400

    Add test for if Deck is a function

---
+++ test/Deck-test.js
@@ -0,0 +1,13 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+const Card = require('../src/Card');
+const Turn = require('../src/Turn');
+
+describe('Deck', function() {
+
+    it('should be a function', function() {
+        const deck = new Deck();
+        expect(Deck).to.be.a('function');
+    })
+});
