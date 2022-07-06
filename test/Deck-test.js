e3194dc78c8103e775a0c41298711565d26bb4ea
Maya Kappen <mayakappen@gmail.com>
2022-07-06 17:16:19 -0400

    Add test to check if instance of Deck, has array

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -9,5 +9,13 @@ describe('Deck', function() {
     it('should be a function', function() {
         const deck = new Deck();
         expect(Deck).to.be.a('function');
-    })
+    });
+    it('should be an instance of Deck', function() {
+        const deck = new Deck();
+        expect(deck).to.be.an.instanceof(Deck)
+    });
+    it('should be an array', function() {
+        const deck = new Deck();
+        expect(deck.deck).to.be.an('array');
+    });
 });
