71309877073225c3b50c2964d23e78694fc5a5d7
Maya Kappen <mayakappen@gmail.com>
2022-07-06 22:03:29 -0400

    Add currentCard test

--- test/Round-test.js
+++ test/Round-test.js
@@ -15,4 +15,22 @@ describe('Round', function() {
         const round = new Round();
         expect(round).to.be.an.instanceof(Round)
     })
+    it('should take in responses', function() {
+        const round = new Round('pandas') 
+        expect(round.response).to.equal('pandas')
+    })
+    it('should record guesses and result', function() {
+        const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+        const round = new Round('object', card);
+        const card2 = new Card(2, 'What is Maya\'s favorite animal?', ['panda', 'frog', 'cat'], 'panda');
+        const round2 = new Round('frog', card2);
+        expect(round2.guesses).to.deep.equal([{answer: 'object', correct: true}, {answer: 'frog', correct: false}])
+    })
+    it('should have currentCard as first in Deck at Round start', function() {
+        const deck = new Deck();
+        const round = new Round();
+        expect(round.currentCard).to.be.an.instanceof('Card');
+        expect(round.currentCard).to.equal(deck.deck[0]);
+    })
+    
 })
