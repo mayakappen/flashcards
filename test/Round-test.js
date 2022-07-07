c2a596767eb382100157af417502beb31f44e184
Maya Kappen <mayakappen@gmail.com>
2022-07-06 21:38:59 -0400

    Add test for if instance of Round

--- test/Round-test.js
+++ test/Round-test.js
@@ -9,6 +9,10 @@ const Round = require('../src/Round');
 describe('Round', function() {
     it('should be a function', function() {
         const round = new Round();
+        expect(round).to.be.a('function')
+    })
+    it('should be an instance of Round', function() {
+        const round = new Round();
         expect(round).to.be.an.instanceof(Round)
     })
 })
