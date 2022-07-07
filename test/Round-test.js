20f1d13431583455d979b751f8dca3c65db8e469
Maya Kappen <mayakappen@gmail.com>
2022-07-06 21:36:54 -0400

    Add test for if Round is a function

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,4 +4,11 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
-const Round = require('../src/Round')
+const Round = require('../src/Round');
+
+describe('Round', function() {
+    it('should be a function', function() {
+        const round = new Round();
+        expect(round).to.be.an.instanceof(Round)
+    })
+})
