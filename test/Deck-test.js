a90a6b9227ab8925f9d19735cbebdc5ac69ba696
Maya Kappen <mayakappen@gmail.com>
2022-07-06 20:48:19 -0400

    Add Deck class

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -3,6 +3,7 @@ const expect = chai.expect;
 
 const Card = require('../src/Card');
 const Turn = require('../src/Turn');
+const Deck = require('../src/Deck');
 
 describe('Deck', function() {
 
@@ -10,19 +11,19 @@ describe('Deck', function() {
         const deck = new Deck();
         expect(Deck).to.be.a('function');
     });
-    it('should be an instance of Deck', function() {
+    it.skip('should be an instance of Deck', function() {
         const deck = new Deck();
         expect(deck).to.be.an.instanceof(Deck)
     });
-    it('should be an array', function() {
+    it.skip('should be an array', function() {
         const deck = new Deck();
         expect(deck.deck).to.be.an('array');
     });
-    it('should initialize with array', function() {
+    it.skip('should initialize with array', function() {
         const deck = new Deck([1, 2, 3]);
         expect(deck.deck).to.equal([1, 2, 3]);
     });
-    it('should know how many cards are in Deck', function() {
+    it.skip('should know how many cards are in Deck', function() {
         const deck = new Deck([1, 2, 3, 4, 5, 6, 7])
         expect(deck.length).to.equal(7);
     });
