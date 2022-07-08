db197d8f3fe271b3588348b251c2be4487a65eb7
Maya Kappen <mayakappen@gmail.com>
2022-07-08 01:05:53 -0400

    Add test for currentRound, start() method

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,13 @@
+const Card = require('./Card');
 const data = require('./data');
+const Round = require('./Round');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = 0
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
@@ -12,6 +16,12 @@ class Game {
 
   printQuestion(round) {
       util.main(round);
+  }
+  start(data) {
+    this.currentRound++
+    const prototypeQuestions = data.prototypeData
+    const questions = prototypeQuestions.map((question) => new Card(question.id, question.questions, question.correctAnswer));
+    return questions
   }
 }
 
